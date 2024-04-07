a8e112a66307bf26b1afdc16f0f8bbb773c6ba69
Michelly Neris <minealves98@gmail.com>
2024-04-07 16:25:27 -0300

    ajustes

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ async def planilha():
 @app.route('/maisvistas')
 async def mais_vistas():
     async with TelegramClient(StringSession(string), api_id, api_hash, timeout=5) as client:
-        top_5 = await top_messages(client)
-        return await render_template('maisvistas.html', top_5=top_5)
+        top_5 = await top_messages()
+        return render_template('maisvistas.html', top_5=top_5)
 
 if __name__ == '__main__':
     app.run()
